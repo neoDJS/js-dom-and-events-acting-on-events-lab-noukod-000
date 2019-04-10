868ecc20b8c238ed88820450f49ef8ff7da8609d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-10 16:25:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much."
+function preventRefreshOnSubmit(){
+    document.querySelector('form').addEventListener('submit', function(event){
+      event.preventDefault()
+    })
+}
+
+preventRefreshOnSubmit()
