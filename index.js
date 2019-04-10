eca104fd1c3fd7f89f42efec71f0d729db0b7f13
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-10 18:11:48 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation(){
+  const val = document.querySelector('input').value; //$("input[name='name']").text('try it');
+  document.querySelector('input').value = "" //$("input[name='name']").text("");
+  return val;
+}
+
+function addNewElementAsLi(){
+  document.querySelector('ul.employee-list').append('<li>'+retrieveEmployeeInformation()+'</li>');
+}
+
+function addNewLiOnClick(){
+  preventRefreshOnSubmit()
+  addNewElementAsLi();
+}
+
+function clearEmployeeListOnLinkClick(){
+  document.querySelector('ul.employee-list').innerHTML = "";
+}
